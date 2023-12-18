13ec0ed6c5d840251d249d3946ce124a0f8f20f5
Siegward-of-Catarina <kurama354@icloud.com>
2023-12-19 03:34:24 +0900

    nnn.

--- VulkanProject/lib/vulkan.cpp
+++ VulkanProject/lib/vulkan.cpp
@@ -8,6 +8,7 @@ namespace lib::renderer
 {
    void vulkan::create_instace()
    {
+      // アプリケーションの詳細設定
       VkApplicationInfo app_info {};
       app_info.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
       app_info.pApplicationName   = "Hello Triangle!";
@@ -20,11 +21,14 @@ namespace lib::renderer
       std::uint32_t glfw_extension_count = 0;
       const char**  glfw_extensions;
       glfw_extensions = glfwGetRequiredInstanceExtensions( &glfw_extension_count );
+      // インスタンス作成の詳細設定
       VkInstanceCreateInfo create_info {};
       create_info.sType             = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
       create_info.pApplicationInfo  = &app_info;
       create_info.enabledLayerCount = 0;    // 今は空
                                             // create_info.ppEnabledLayerNames
+
+                                            // OSによって拡張機能の追加が生じるので分岐
 #if defined( _WIN32 )
       create_info.enabledExtensionCount   = glfw_extension_count;
       create_info.ppEnabledExtensionNames = glfw_extensions;
@@ -42,49 +46,45 @@ namespace lib::renderer
       create_info.enabledExtensionCount   = static_cast<uint32_t>( required_extensions.size() );
       create_info.ppEnabledExtensionNames = required_extensions.data();
 #endif
-
+      // 拡張機能がサポートされているか確認
       check_vk_extension_supported( glfw_extensions, glfw_extension_count );
+
       // ようやく生成
       if ( vkCreateInstance( &create_info, nullptr, &vk_instance ) != VK_SUCCESS )
       {
          throw std::runtime_error( "failed to create instance!" );
       }
    }
+
    void vulkan::check_vk_extension_supported( const char** extensions, const std::uint32_t count )
    {
-      std::uint32_t
-        // 全てのサポートしている拡張機能の数のみ取得
-        extension_count = 0;
+      // 全てのサポートしている拡張機能の数のみ取得
+      std::uint32_t extension_count = 0;
       vkEnumerateInstanceExtensionProperties( nullptr, &extension_count, nullptr );
 
       // 拡張機能の情報を取得
       std::vector<VkExtensionProperties> supported_extensions( extension_count );
       vkEnumerateInstanceExtensionProperties( nullptr, &extension_count, supported_extensions.data() );
 
-      std::string res;
+      // 拡張機能の名前を一つにまとめる
+      std::string supported_extension_names;
       for ( const auto& extension : supported_extensions )
       {
-         res += extension.extensionName;
-         res += "\n";
+         supported_extension_names += extension.extensionName;
+         supported_extension_names += "\n";
       }
 
+      //拡張機能がサポートされているか一覧からチェック
       for ( std::uint32_t i = 0; i < count; i++ )
       {
-         // std::cout << extensions[ i ] << std::endl;
-         if ( res.find( extensions[ i ] ) != std::string::npos ) 
-         { 
-             std::cout << extensions[ i ] << " is supported" << std::endl; 
-         }
-         else { std::cout << extensions[ i ] << " is not supported" << std::endl;
-         }
+         if ( supported_extension_names.find( extensions[ i ] ) != std::string::npos )
+            std::cout << extensions[ i ] << " is supported" << std::endl;
+         else throw std::runtime_error( std::string { extensions[ i ] } + std::string { " is not supported" } );
       }
    }
    void vulkan::init() { create_instace(); }
    void vulkan::release() {}
-   vulkan::vulkan() : vk_instance {VK_NULL_HANDLE}
-   {
-
-   }
+   vulkan::vulkan() : vk_instance { VK_NULL_HANDLE } {}
 }    // namespace lib::renderer
 
 namespace api::renderer
