5ae313daa589a6808bf4fcb82e13dfeca615a812
Siegward-of-Catarina <kurama354@icloud.com>
2024-08-04 00:15:05 +0900

    Revert "swapchainの情報を取得する関数を作成."
    This reverts commit 8a3f329d11e21d5caedac517636b1f903791af9e.

--- VulkanProject/lib/vulkan/swap_chain_support.hpp
+++ VulkanProject/lib/vulkan/swap_chain_support.hpp
@@ -12,13 +12,7 @@ namespace my_library::vulkan::swapchain_support
 
       const bool
       adequate();
-      const vk_surface_format_khr
-      choose_swap_surfaceformat( const vk::Format&        format = vk::Format::eB8G8R8A8Srgb,
-                                 const vk::ColorSpaceKHR& colorspace = vk::ColorSpaceKHR::eSrgbNonlinear );
-      const vk_present_mode_khr
-      choose_swap_presentmode( const vk::PresentModeKHR& presentmode = vk::PresentModeKHR::eMailbox );
    };
    const details
    query_details( const vk_physicaldevice& device, const unq_vk_surface& surface );
-
-}    // namespace my_library::vulkan::swapchain_support
+}    // namespace my_library::vulkan::swap_chain_support
